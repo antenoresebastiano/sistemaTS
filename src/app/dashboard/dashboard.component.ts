b6e0ed5964df540bfae93af695b265e59924975a
fk0074 <sebastiano.gugliandolo@consulenti.allitude.it>
2025-12-09 15:30:46 +0100

    Messaggio del commit

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -6,7 +6,8 @@ import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatNativeDateModule } from '@angular/material/core';
-
+import jsPDF from 'jspdf';
+import autoTable from 'jspdf-autotable';
 
 @Component({
   selector: 'app-dashboard.component',
@@ -28,22 +29,37 @@ export class DashboardComponent implements OnInit {
   constructor(private http: HttpClient) {}
 
   username = 'Rosaci Carmela';
-  codiceFiscale = 'RSCCML58A44F112E';
-  indirizzo = "Via Mar Nero 3/B Milano";
+  codiceFiscaleMedico = 'RSCCML58A44F112E';
+  indirizzoMedico = "Via Mar Nero 3/B Milano";
+
+  codiceFiscalePaziente = '';
+  indirizzoPaziente = "";
+  nomeCompletoPaziente = "";
+  
   filtro = "A";
   listaOpzioni: any[] = [];
   opzioneSelezionata : any[] = [];
 
-  dataSelezionata: Date | null = null
-
-  valoreSelezionato: string = '';
-
-lista = [
-  { valore: 'Certificato Anamnestico per patente', descrizione: 'Certificato Anamnestico per patente' },
-  { valore: 'Certificato Invalidità', descrizione: 'Certificato Invalidità' },  
-];
-
+   valoreSelezionato: string = '';
+   importoSelezionato : string = '100 €';
   
+  dataSelezionata: Date = new Date();
+  numFattura : string = "10";
+  valoreSelezionatoImporto = "";
+
+
+  lista = [
+    { valore: 'Certificato Anamnestico per patente', descrizione: 'Certificato Anamnestico per patente' },
+    { valore: 'Certificato Invalidità', descrizione: 'Certificato Invalidità' },  
+  ];
+
+
+  listaImporto = [
+    { valore: '100 €', descrizione: '100 €' },
+    { valore: '50 €', descrizione: '50 €' },
+    { valore: '130 €', descrizione: '130 €' },
+  ];
+
 
     /*ngOnInit() {
       this.http.get<any[]>('assets/clienti.json').subscribe((data: any[]) => {
@@ -62,7 +78,7 @@ lista = [
 
     
     onLogin() {
-      alert("Iiii");
+      
     }
 
  
@@ -83,13 +99,53 @@ lista = [
     onPazienteChange() {
       const paziente = this.listaOpzioni.find(p => p.id == this.opzioneSelezionata);
       
-
       console.log("Paziente selezionato:", paziente); // DEBUG
     
       if (paziente) {
-        this.codiceFiscale = paziente.CodiceFiscale;
-        this.indirizzo = paziente.Indirizzo;
+        this.codiceFiscalePaziente = paziente.CodiceFiscale;
+        this.indirizzoPaziente = paziente.Indirizzo + " " + paziente.Comune;
+        this.nomeCompletoPaziente = paziente.NomeCompleto
       }
+    }
+
+    generaPdf() {
+      const doc = new jsPDF();
+
+      // Titolo
+      doc.setFontSize(16);
+      doc.text('Fattura', 10, 10);
+  
+      // Dati base
+      doc.setFontSize(12);
+      doc.text(`Numero fattura: ${this.numFattura}         ${this.dataSelezionata.toLocaleDateString()}`  , 10, 20);
+
+      doc.text(`Dott.ssa : ${this.username}  ${this.codiceFiscaleMedico}` , 10, 30);
+      doc.text(`${this.indirizzoMedico} ` , 10, 40);
+      
+      
+      
+      doc.text(`Paziente: ${this.nomeCompletoPaziente}`, 10, 70);
+      doc.text(`Codice Fiscale: ${this.codiceFiscalePaziente}`, 10, 80);
+      doc.text(`Indirizzo: ${this.indirizzoPaziente}`, 10, 90);
+
+      doc.text(` ${this.valoreSelezionato}`, 10, 110);
+      doc.text(`Importo  ${this.importoSelezionato}`, 10, 120);
+      //doc.text(`Totale: ${this.totale} €`, 10, 50);
+  
+      // (opzionale) tabella
+      autoTable(doc, {
+        startY: 60,
+       // head: [['Voce', 'Q.tà', 'Prezzo', 'Totale']],
+        body: [
+          // esempio statico, puoi sostituirlo con array del form
+         // ['Prodotto A', '2', '10 €', '20 €'],
+         // ['Prodotto B', '1', '15 €', '15 €']
+        ]
+      });
+  
+      // Scarica il PDF
+      doc.save(`fattura_${this.numFattura || 'senza_numero'}.pdf`);
+
     }
 
     
